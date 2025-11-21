0ea37275098f0fe09292cdc44bf471326af2ac36
Ashutosh Upadhyay <43708500+AshutoshUpadhyayGit@users.noreply.github.com>
2025-11-21 13:18:26 +0530

    testing 2

--- app.py
+++ app.py
@@ -1825,6 +1825,16 @@ def watchman_observe():
 
 # ---------------------- MAIN ---------------------- #
 if __name__ == '__main__':
+    import psycopg2, os
+
+    url = os.getenv("SUPABASE_DB_URL")
+
+    try:
+        conn = psycopg2.connect(url)
+        print("CONNECTED")
+    except Exception as e:
+        print("FAILED:", e)
+        
     app.run(debug=True)
 
 
