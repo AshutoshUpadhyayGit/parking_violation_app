7cfa55eb0f3acd0f402d0ca90e54207277901a73
Ashutosh Upadhyay <43708500+AshutoshUpadhyayGit@users.noreply.github.com>
2025-11-22 12:35:54 +0530

    Revert "chekcing"
    This reverts commit 1c360e5a444a5633e9686ac3f399c959a8712b72.

--- app.py
+++ app.py
@@ -515,8 +515,6 @@ def admin_dashboard():
                 how='left'
             )
 
-            print("My Pending Record : \n", pending_df.to_dict(orient='records'))
-
             actioned_df = df[df['Status'].isin(['Verified', 'Dismissed','CLAMPED'])]
             clamped_df = df[df['Status'].eq('UNKNOWN')]
 
